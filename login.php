f4368d7819c5edb003b0506a6d3b3d76cf83a7bf
gartic99 <artic.1804049@studenti.uniroma1.it>
2020-05-15 23:40:10 +0200

    password fix

--- login.php
+++ login.php
@@ -20,16 +20,18 @@
     $email=$_POST["email"];
     $password=$_POST["password"];
     //ricerco la password nel db corrispondente alla email
-    $q1="select band.password from band where band.mail=$1";
-    $q2="select locale.password from locale where locale.mail = $1";
-    $resultBand = pg_query_params($con,$q1,array($email));
-    $resultLocale = pg_query_params($con,$q2,array($email));
+    $q1="select band.password from band where band.mail=$1 and password=$2";
+    $q2="select locale.password from locale where locale.mail = $1 and password=$2";
+    $resultBand = pg_query_params($con,$q1,array($email,md5($password)));
+    $resultLocale = pg_query_params($con,$q2,array($email,md5($password)));
 
     $lineLocale=pg_fetch_array($resultLocale,null,PGSQL_ASSOC);
     $lineBand=pg_fetch_array($resultBand,null,PGSQL_ASSOC);
     //verifico che ci siano dei risultati per il locale
     if(!pg_num_rows($resultLocale)==0){
-        if(strcmp($lineLocale["password"],md5($password))===0){ //verifico che la password corrisponda con quella inserita
+        /*echo $lineLocale['password'];
+        echo md5($password);
+        if(strcmp($lineLocale["password"],md5($password))==0){ //verifico che la password corrisponda con quella inserita
             $qn="select locale.nome from locale where locale.mail=$1";
             $resultName = pg_query_params($con,$qn,array($email));
             $ln=pg_fetch_array($resultName,null,PGSQL_ASSOC);
@@ -40,11 +42,17 @@
             echo "<h1>Errore Password</h1>
             <a href=../login.html>clicca qui per login
             </a>";
-        }
+        }*/
+        $qn="select locale.nome from locale where locale.mail=$1";
+        $resultName = pg_query_params($con,$qn,array($email));
+        $ln=pg_fetch_array($resultName,null,PGSQL_ASSOC);
+        $_SESSION["username"] = $ln["nome"];
+        header("location: profilo_locale.php"); //carico il profilo dell'utente
+
     }
     //verifico che ci siano dei risultati per la band
     else if(!pg_num_rows($resultBand)==0){
-        if(strcmp($lineBand["password"],md5($password)===0)){ //verifico che la password corrisponda con quella inserita
+        /*if(strcmp($lineBand["password"],md5($password)==0)){ //verifico che la password corrisponda con quella inserita
             $qn="select band.nome from band where band.mail=$1";
             $resultName = pg_query_params($con,$qn,array($email));
             $ln=pg_fetch_array($resultName,null,PGSQL_ASSOC);
@@ -54,10 +62,16 @@
         else{
             echo "<h1>Errore Password</h1>
             <a href=../login.html>clicca qui per login</a>";
-        }
+        }*/
+        $qn="select band.nome from band where band.mail=$1";
+        $resultName = pg_query_params($con,$qn,array($email));
+        $ln=pg_fetch_array($resultName,null,PGSQL_ASSOC);
+        $_SESSION["username"] = $ln["nome"];
+        header("location: profilo_band.php"); //carico il profilo dell'utente
+
     }
     else{
-        echo "<h1>Errore Username, Sicuro di essere registrato?</h1>
+        echo "<h1>Errore Username o Password</h1>
         <a href=../login.html>clicca qui per login</a>";
     }
     
