bbbc0f0dedc0b0bed16547114b9dda444b9265c2
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-15 03:29:53 +0200

    Pulizia generale & nuova nav bar con Bootstrap

--- profilo_band.php
+++ profilo_band.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="profili.css">
+    <link rel="stylesheet" href="profiles.css">
     <title>Band Advisor</title>
     <link rel="icon" href="assets/B.png">
 </head>
