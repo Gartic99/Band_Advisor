fb37616a9774c6ad60e966380a7d3082f5a85ed6
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-15 21:20:45 +0200

    Misc fixes

--- profilo_band.php
+++ profilo_band.php
@@ -8,6 +8,9 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="profiles.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <title>Band Advisor</title>
     <link rel="icon" href="assets/B.png">
 </head>
@@ -23,6 +26,8 @@ session_start();
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Scrivi una Recensione<span class="sr-only">(current)</span></a>
+            </li>
+            <li>
                 <a class="nav-link" href="#">Contatta un Locale<span class="sr-only">(current)</span></a>
             </li>
           </ul>
@@ -56,12 +61,7 @@ session_start();
                 </div>
                 <div class="col-4">
                     <div class="container">
-                        <div class="row">
-                            <a class="contatta" href="#">
-                                Contatta un Locale
-                            </a>
-                        </div>
-                        <div class="row" style="height: 30vh;"></div>
+                        <div class="row" style="height: 10vh;"></div>
                         <div class="row">
                             <div class="col-auto">
                                 <div class="recensioni">Le tue recensioni</div>
