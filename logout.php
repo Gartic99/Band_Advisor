00eaf8f3eec1187e3c7c8b330ee01850a904e109
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-21 14:27:30 +0200

    Revert "Delete logout.php"
    This reverts commit efd9e1d6c0316d7f9be92dae779516880d451fd5.

---
+++ logout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+</head>
+<body>
+    <?php
+        function logout(){
+            $_SESSION = array();
+            session_destroy();
+        }
+    ?>
+</body>
+</html>
