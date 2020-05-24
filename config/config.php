7bb42cea38215c16346018bc544cab9035f5f71a
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-24 22:12:03 +0200

    Inizio file config

---
+++ config/config.php
@@ -0,0 +1,8 @@
+/** Il nome del database  */
+define('DB_NAME', 'postgres');
+/** Nome utente del database  */
+define('DB_USER', 'postgres');
+/** Password del database */
+define('DB_PASSWORD', 'Quindicimaggio_20');
+/** Hostname  */
+define('DB_HOST', 'database-1.csh3ixzgt0vm.eu-west-3.rds.amazonaws.com');
