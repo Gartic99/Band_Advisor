4af06a3d8e3a3e16a242e32b4fe391ede79d1804
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-25 19:54:57 +0200

    Edit.php fix [WIP]

--- modal/edit.php
+++ modal/edit.php
@@ -1,19 +1,28 @@
 <?php
+    include " ../config/config.php";
     if (!isset($_POST["profile_img"])) {
         if ($_FILES["profile_img"]["error"] > 0) {
             echo "Error: immagine non carrciata correttamente <br>";
         } else {
             move_uploaded_file($_FILES["profile_img"]["tmp_name"], $_FILES["file"]["name"]);
 
-            $bin_string = file_get_contents($_FILES["file"]["name"]);
+            $bin_string = file_get_contents($_FILES["profile_img"]["name"]);
             $hex_string = base64_encode($bin_string);
-            $mysqli = mysqli_init();
 
-            if (!$mysqli->real_connect('localhost', 'root', '', 'arihant')) {
-                die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+            $host = $DB_HOST;
+            $user = $DB_USER;
+            $pass = $DB_PASSWORD;
+            $db =   $DB_NAME;
+
+            //apro la connessione con il db postgress
+            $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+            or die ("Could not connect to server\n");
+            if (!$con){
+                echo "<h1> Impossibile connettersi</h1>";
             }
-
-            $mysqli->query("INSERT INTO upload(image) VALUES ('" . $hex_string . "')");
+           // $mysqli->query("INSERT INTO upload(image) VALUES ('" . $hex_string . "')");
+            $q="insert into img_profili(mail,image,desc) values {$_COOKIE["mail"]},{$hex_string},{$_POST["desc"]}";
+            $result=pg_query($con,$q);
         }
     }
 ?>
@@ -21,7 +30,7 @@
 
 
 <?php
-$target_dir = "uploads/";
+/*$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -67,5 +76,5 @@ if ($uploadOk == 0) {
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
-}
+}*/
 ?>
