523e1f6019d76ae6e39f68d51d64199873fa00ae
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-29 19:08:18 +0200

    Manca un pezzettino per implementare l'auto comp

--- config/utils.php
+++ config/utils.php
@@ -47,4 +47,36 @@
            return pg_fetch_array( $result2 ,null ,PGSQL_ASSOC)["mail"];
         }
     }
+    function getName($band){
+        $host = constant("DB_HOST");
+        $user = constant("DB_USER");
+        $pass = constant("DB_PASSWORD");
+        $db =   constant("DB_NAME");
+
+        //apro la connessione con il db postgress
+        $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+        or die ("Could not connect to server\n");
+        if (!$con){
+            echo "<h1> Impossibile connettersi</h1>";
+        }
+        
+        $arg=$_POST["arguments"];
+        if(empty($arg)){
+            return "Caio";
+        }
+        $q1="SELECT band.nome FROM band WHERE band.mail=$1";
+        $result=pg_query_params($con,$q1,array($_POST["arguments"][0]));
+        
+        if(pg_num_rows($result)>0){
+           return pg_fetch_array( $result ,null ,PGSQL_ASSOC)["nome"];
+        }
+
+        $q2="SELECT locale.nome FROM locale WHERE locale.mail=$1";
+        $result2=pg_query_params($con,$q2,array($_POST["arguments"][0]));
+        
+        if(pg_num_rows($result2)>0){
+           return pg_fetch_array( $result2 ,null ,PGSQL_ASSOC)["nome"];
+        }
+    }
+    
 ?>
