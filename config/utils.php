a2587514ff15cd7fbe23b6a1aec1d209e07a6f20
IlKaiser <samuele.rinaldi90@gmail.com>
2020-05-29 17:38:00 +0200

    Fix isBand

--- config/utils.php
+++ config/utils.php
@@ -15,8 +15,8 @@
         $q1="SELECT band.nome,band.mail FROM band WHERE band.mail=$1";
         $result=pg_query_params($con,$q1,array($band));
         
-        if(pg_num_rows($result)==0){
-        return true;
+        if(pg_num_rows($result)>0){
+            return true;
         }
         return false;
     }
