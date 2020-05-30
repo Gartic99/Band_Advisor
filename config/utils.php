ada6706203ef1b4be8e9ed3da9c0b84c2ab6fd07
gartic99 <artic.1804049@studenti.uniroma1.it>
2020-05-30 18:53:30 +0200

    fix contatta e utils.php

--- config/utils.php
+++ config/utils.php
@@ -4,9 +4,10 @@
         $action = $_POST['action'];
         switch($action) {
             case 'isBand' : isBand($_POST['arguments']);break;
+            case 'isLocale' : isLocale($_POST['arguments']);break;
             case 'getMail' : getMail($_POST['arguments']);break;
             case 'getName' : getName($_POST['arguments']);break;
-            // ...etc...
+            case 'getNameString' : getNameString($_POST['arguments']);break;
         }
     }
     function isBand($band){
@@ -30,6 +31,28 @@
         }
         return false;
     }
+    function isLocale($locale){
+        $host = constant("DB_HOST");
+        $user = constant("DB_USER");
+        $pass = constant("DB_PASSWORD");
+        $db =   constant("DB_NAME");
+
+        //apro la connessione con il db postgress
+        $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+        or die ("Could not connect to server\n");
+        if (!$con){
+            echo "<h1> Impossibile connettersi</h1>";
+        }
+        
+        $q1="SELECT locale.nome,locale.mail FROM locale WHERE locale.mail=$1";
+        $result=pg_query_params($con,$q1,array($band));
+        
+        if(pg_num_rows($result)>0){
+            return true;
+        }
+        return false;
+
+    }
     function getMail($band){
         $host = constant("DB_HOST");
         $user = constant("DB_USER");
@@ -47,17 +70,50 @@
         $result=pg_query_params($con,$q1,array($band));
         
         if(pg_num_rows($result)>0){
-           echo pg_fetch_array( $result ,null ,PGSQL_ASSOC)["mail"];
+           return pg_fetch_array( $result ,null ,PGSQL_ASSOC)["mail"];
         }
 
         $q2="SELECT locale.mail FROM locale WHERE locale.nome=$1";
         $result2=pg_query_params($con,$q2,array($band));
         
         if(pg_num_rows($result2)>0){
-           echo pg_fetch_array( $result2 ,null ,PGSQL_ASSOC)["mail"];
+           return pg_fetch_array( $result2 ,null ,PGSQL_ASSOC)["mail"];
         }
     }
     function getName($band){
+        $host = constant("DB_HOST");
+        $user = constant("DB_USER");
+        $pass = constant("DB_PASSWORD");
+        $db =   constant("DB_NAME");
+
+        //apro la connessione con il db postgress
+        $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+        or die ("Could not connect to server\n");
+        if (!$con){
+            echo "<h1> Impossibile connettersi</h1>";
+        }
+        
+        $arg=$_POST["arguments"];
+        if(empty($arg)){
+            echo "Caio";
+        }
+        $q1="SELECT band.nome FROM band WHERE band.mail=$1";
+        $result=pg_query_params($con,$q1,array($_POST["arguments"]));
+        
+        if(pg_num_rows($result)>0){
+            return pg_fetch_array( $result ,null ,PGSQL_ASSOC)["nome"];
+        }
+
+        $q2="SELECT locale.nome FROM locale WHERE locale.mail=$1";
+        $result2=pg_query_params($con,$q2,array($_POST["arguments"]));
+        
+        if(pg_num_rows($result2)>0){
+            return pg_fetch_array( $result2 ,null ,PGSQL_ASSOC)["nome"];
+        }
+    }
+
+
+    function getNameString($band){
         $host = constant("DB_HOST");
         $user = constant("DB_USER");
         $pass = constant("DB_PASSWORD");
