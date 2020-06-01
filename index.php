3db9bee00afc55b5ffc7ade6676d913c438eaabf
IlKaiser <samuele.rinaldi90@gmail.com>
2020-06-01 18:44:42 +0200

    Fix && JSON per indexing

--- index.php
+++ index.php
@@ -12,6 +12,24 @@ session_start();
     
     <!-- Google Index Verification -->
     <meta name="google-site-verification" content="aqksowhR4CMGpoNHD1zI-vqWzeK1CmZOEbbNfTgYAGc" />
+    <!-- Indexing google -->
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Login",
+        "item": "https://bandadvisor.it/login/login.html"
+      },{
+        "@type": "ListItem",
+        "position": 2,
+        "name": "Signup",
+        "item": "https://example.com/signup/signup.html"
+      }]
+    }
+    </script>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
