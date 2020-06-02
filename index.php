d3a28053b87ff8a042903cdc51ea8af12780789b
IlKaiser <samuele.rinaldi90@gmail.com>
2020-06-02 17:45:32 +0200

    Speedup pt. 3

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ session_start();
     <meta name="google-site-verification" content="aqksowhR4CMGpoNHD1zI-vqWzeK1CmZOEbbNfTgYAGc" />
       
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+   
 
     <script src="/js/cookie.js"></script>
 
