d561b2fc15e558a2c7b7e0866edaaa6fae2df892
IlKaiser <samuele.rinaldi90@gmail.com>
2020-06-03 20:37:14 +0200

    Meta ovunque

--- main/search.php
+++ main/search.php
@@ -5,7 +5,10 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Il primo sito che mette in diretto contatto artisti emergenti e locali">
+    <meta name="keywords" content="bandadvisor band emergenti locali">
+    <meta name="author" content="Ghenadie Artic,Marco Calamo">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/profilo/profiles.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
